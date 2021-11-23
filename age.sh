8613fb4300309af14211ef661c1e04c443086664
breinerAlvarez <alvarezbreiner02@gmail.com>
2021-11-22 20:33:52 -0500

    Creé un archivo de ejemplo

--- age.sh
+++ age.sh
@@ -1,4 +1,47 @@
+#Guess the number - Shell Script#
 read -p "What's your name? " NAME
 read -p "How old are you? " AGE
 
-cowsay -f tux "You are $NAME, and you are $AGE years old" | lolcat
+cowsay -f tux "So, you're name is $NAME, and you are $AGE years old, huh?" | lolcat
+read -p "" STOP
+clear
+
+echo "Cool, now let's play a game"
+echo "Rules: You have to guess a number between 1 and 10 whitin 3 opportunities"
+
+NUMBER=$(($RANDOM % 10 + 1))
+
+LIVES=(1 2 3)
+
+for LIFE in "${LIVES[@]}"; do
+	LEFT=$((3 - $LIFE))
+	read -p "Type your guess: " GUESSEDNUMBER
+	if [ $GUESSEDNUMBER -eq $NUMBER ]; then
+		cowsay -f dragon "Well done $NAME you got it" | lolcat
+		break
+	elif [ $GUESSEDNUMBER -gt $NUMBER ]; then
+		if [ $LEFT -eq 0 ]; then
+			cowsay "Sorry $NAME, you've lost. The number was $NUMBER" | lolcat
+			break
+		fi
+
+		if [ $LEFT -eq 1 ]; then
+			echo "No way, you have $LEFT life remaining"
+		else
+			echo "No way, you have $LEFT lives remaining"
+		fi
+		echo "Hint: the number is lower than $GUESSEDNUMBER"
+	else
+		if [ $LEFT -eq 0 ]; then
+			cowsay "Sorry $NAME, you've lost. The number was $NUMBER" | lolcat
+			break
+		fi
+
+		if [ $LEFT -eq 1 ]; then
+			echo "No way, you have $LEFT life remaining"
+		else
+			echo "No way, you have $LEFT lives remaining"
+		fi
+		echo "Hint: the number is greater than $GUESSEDNUMBER"
+	fi
+done
---
+++ example.sh
@@ -0,0 +1,2 @@
+my_array=(foo bar baz)
+for index in "${!my_array[@]}"; do echo "$index"; done
